8c1431cfc5d0647aa8110d75e54dfc8310945cd9
thecakeisalie25 <larsona@outlook.com>
2017-12-20 16:41:20 -0600

    Added a few commands.
    Fixed status
    added members
    added whoami - WIP

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require ('./config.json');
+const {prefix, token} = require ('./config.json');
 // When ON log to console.
 client.on('ready', () => {
     console.log('---Alright, we\'re up and running!---')
@@ -8,9 +8,13 @@ client.on('ready', () => {
 
 client.on('message', message => {
     // console.log(message.content);
-    if (message.content === 'ok.status'){
-        message.channel.send('i\'m doin ok.')
+    if (message.content === `${prefix}status`){
+        message.channel.send(`i\'m doin ok.`);
+    }else if (message.content === `${prefix}members`) {
+        message.channel.send(`${message.guild.name} has (${message.guild.memberCount}) members`);
+    }else if (message.content === `${prefix}whoami`){
+        message.channel.send(`You are ${message.author.username}\nAnd your ID is ${message.author.id}`);
     }
 })
 
-client.login(config.token);
+client.login(token);

0b0978115f47eb79f13922a3e7c409acfbbf2c01
thecakeisalie25 <larsona@outlook.com>
2017-12-19 23:18:06 -0600

    implement message log

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+// When ON log to console.
 client.on('ready', () => {
     console.log('Alright, we\'re up and running!')
 });
 
-client.login('MzkyMDkzNzg0NzgwODMyNzY4.DRuCBQ._0nwbTTceac3PRhecXyJa3lGnbk')
+client.on('message', message => {
+    console.log(message.content);
+})
+
+client.login('MzkyMDkzNzg0NzgwODMyNzY4.DRuCBQ._0nwbTTceac3PRhecXyJa3lGnbk');

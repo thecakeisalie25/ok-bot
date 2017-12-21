f7eb2844cfd1424f0bd9cd36c8654d6c4e00790e
thecakeisalie25 <larsona@outlook.com>
2017-12-20 23:13:35 -0600

    give up on game, wip kill

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
     // console.log(message.content);
     if (message.content === `${prefix}status`){
         message.channel.send(`i\'m doin ok.`);
@@ -14,9 +19,9 @@ client.on('message', message => {
         message.channel.send(`${message.guild.name} has (${message.guild.memberCount}) members`);
     }else if (message.content === `${prefix}whoami`){
         message.channel.send(`You are ${message.author.username}\nAnd your ID is ${message.author.id}`);
-    }else if (message.content.startsWith(`${prefix}game`)){
-        message.channel.send(`:${message.content}`);
-        console.log(`game command rec`);
+    }else if (message.content === `${prefix}kill`){
+        if (message.author.id === `134509976956829697`){message.channel.send(`temp.nokill`);}
+        else {message.channel.send(`fuck off`);}
     }
 })
 
